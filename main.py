c1fd4429c13bbf274a484b40f18eb114ab4f5b27
LeoDelgado23 <73501369+LeoDelgado23@users.noreply.github.com>
2024-08-18 20:27:52 -0600

    Modified app: Uploading New Resource - Sync by Date Range.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import List, Dict, Any
 import json
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Initializing the application server.
 app = FastAPI()
@@ -23,6 +23,10 @@ class SyncTwoDates_Response(BaseModel):
     message: str
     operations: List[Response]
 
+class SyncDateRange_Response(BaseModel):
+    range: str
+    results: List[SyncTwoDates_Response]
+
 # Function use for loading JSONL files into variables.
 def load_files(path: str) -> List[Files]:
     file_path = f"dms-responses/{path}.jsonl"
@@ -37,13 +41,15 @@ def load_files(path: str) -> List[Files]:
         raise HTTPException(status_code = 500, detail = f"An error occurred >> Executing Load Files Function : {str(e)}")
 
 # Function use for validating dates structure and logic.
-def validate_dates(start_date: str, end_date: str):
+def validate_dates(start_date: str, end_date: str) -> List[datetime]:
     try:
         start = datetime.strptime(start_date, "%Y-%m-%d")
         end = datetime.strptime(end_date, "%Y-%m-%d")
         
         if start >= end:
             raise HTTPException(status_code = 400, detail = "End date must be later than start date.")
+        else:
+          return [start, end]
     except ValueError:
         raise HTTPException(status_code = 400, detail = "Invalid date format. Use YYYY-MM-DD.")
 
@@ -104,7 +110,7 @@ def search_update_ops(start_dict: Dict[str, Files], end_dict: Dict[str, Files]) 
 # Exposing GET endpoint that compares two dates.
 @app.get("/syncTwoDates")
 def sync_files_two_dates(start_date: str, end_date: str) -> SyncTwoDates_Response:
-    validate_dates(start_date, end_date)
+    [_, _] = validate_dates(start_date, end_date)
 
     start_files = load_files(start_date)
     end_files = load_files(end_date)
@@ -118,3 +124,32 @@ def sync_files_two_dates(start_date: str, end_date: str) -> SyncTwoDates_Respons
     operations.extend(search_update_ops(start_dict, end_dict))
 
     return {"message": f"Operations made from {start_date} to {end_date}.","operations": operations}
+
+# Exposing GET endpoint that compares a range of dates.
+@app.get("/syncDateRange")
+def sync_files_date_range(start_date: str, end_date: str) -> SyncDateRange_Response:
+    [start, end] = validate_dates(start_date, end_date)
+    
+    date_ahead = start + timedelta(days = 1)
+    results = []
+
+    while date_ahead <= end:
+        operations = []
+        next_date_str = date_ahead.strftime("%Y-%m-%d")
+        curr_date_str = (date_ahead - timedelta(days = 1)).strftime("%Y-%m-%d")
+
+        next_files = load_files(next_date_str)
+        curr_files = load_files(curr_date_str)
+
+        next_dict = {file.id: file for file in next_files}
+        curr_dict = {file.id: file for file in curr_files}
+        
+        operations.extend(search_create_ops(curr_dict, next_dict))
+        operations.extend(search_delete_ops(curr_dict, next_dict))
+        operations.extend(search_update_ops(curr_dict, next_dict))
+
+        results.append({"message": f"Operations made from {curr_date_str} to {next_date_str}.","operations": operations})
+
+        date_ahead += timedelta(days = 1)
+
+    return {"range": f"Range operations made from {start_date} to {end_date}.","results": results}
